8e42a889f1d368cbcf62e2f57cb4f0d80c027325
mutiurrahman_khan <mutiurrahman47@gmail.com>
2024-12-03 14:56:31 +0530

    Search an elemet in the singly LinkedList

--- src/LinkedList/Demo1SigleLinkedList.java
+++ src/LinkedList/Demo1SigleLinkedList.java
@@ -20,12 +20,4 @@ public class Demo1SigleLinkedList {
         System.out.println();
     }
 }
-class Node{
-    int data;
-    Node next;
 
-    public Node(int  data){
-        this.data = data;
-        this.next = null;
-    }
-}
---
+++ src/LinkedList/Demo2_LinkedList_SearchElement.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Demo2_LinkedList_SearchElement {
+    public static void main(String[] args) {
+        int numSearch;
+        Scanner scan = new Scanner(System.in);
+        Node head =  new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next =new Node(40);
+        System.out.println("Enter the  number you want to search in the linkedlist...");
+        numSearch = scan.nextInt();
+//        SearchElementInLinkedList.searchElementInLinkedListMethod(head,numSearch);
+        System.out.println(SearchElementInLinkedList.searchElementInLinkedListMethod(head,numSearch));
+    }
+}
+class SearchElementInLinkedList{
+    public static boolean searchElementInLinkedListMethod(Node head,int numSearch){
+        while(head != null){
+            System.out.println(head.data + " ----> ");
+            if(numSearch == head.data){
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
